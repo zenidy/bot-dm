886630b4c15d044d630a7976536aba4d48de762d
Zen <pcnanotechco@gmail.com>
2025-12-06 12:23:06 -0600

    refreshed comments

--- deploy-commands.js
+++ deploy-commands.js
@@ -37,14 +37,14 @@ const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
   try {
-    console.log("Refreshing application (/) commands...");
+    console.log("Refreshing application");
 
     await rest.put(
       Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
       { body: commands }
     );
 
-    console.log("Successfully reloaded application (/) commands.");
+    console.log("Successfully reloaded application");
   } catch (error) {
     console.error(error);
   }
